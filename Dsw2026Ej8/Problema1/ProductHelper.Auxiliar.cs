253936a929ac0da56b36c9419b68a219258137a9
Catalina Herrera <hcatalina152@gmail.com>
2026-05-03 16:53:55 -0300

    Problema 1 resuelto

---
+++ Dsw2026Ej8/Problema1/ProductHelper.Auxiliar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema1
+{
+    public partial class ProductHelper
+    {
+        private string SimboloMoneda()
+        {
+            return "$";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Dsw2026Ej8
+﻿using Dsw2026Ej8.Problema1;
+
+namespace Dsw2026Ej8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //------------ Problema 1 ------------
+            ProductHelper product = new ProductHelper();
+
+            long code = 2005;
+            string description = "ACER Ryzen 7";
+            decimal price = 1500.10m;
+
+            string etiqueta = product.ObtenerEtiquetaProducto(code, description, price);
+
+            Console.WriteLine("Ejercicio 1: Etiqueta");
+            Console.WriteLine(etiqueta);
         }
     }
 }
