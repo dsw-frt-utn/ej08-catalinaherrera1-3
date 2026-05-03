f066a4966f46b2a0da9fce9f4eec3e53ccdefebe
Catalina Herrera <hcatalina152@gmail.com>
2026-05-03 17:14:33 -0300

    Problema 2 resuelto

---
+++ Dsw2026Ej8/Ejercicio2/Problema2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2026Ej8.Ejercicio2
+{
+    public class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total;
+
+            if (quantity > 0) { 
+                total = quantity * unitPrice;
+            }
+            else
+            {
+                 total = 0;
+            }
+
+            var ventaAnonymous = new { Code = productCode, Description = productDescription, Quantity = quantity, Total = total };
+
+            return $"{ventaAnonymous.Code}-{ventaAnonymous.Description}-{ventaAnonymous.Total}";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,8 +1,11 @@
-﻿using Dsw2026Ej8.Problema1;
+﻿using System;
+using Dsw2026Ej8;
+using Dsw2026Ej8.Problema1;
+using Dsw2026Ej8.Ejercicio2;
 
 namespace Dsw2026Ej8
 {
-    internal class Program
+     class Program
     {
         static void Main(string[] args)
         {
@@ -17,6 +20,24 @@ namespace Dsw2026Ej8
 
             Console.WriteLine("Ejercicio 1: Etiqueta");
             Console.WriteLine(etiqueta);
+
+            //------------ Problema 2 ------------
+            Problema2 venta = new Problema2();
+
+            long productCode = 1223;
+            string productDescription = "Teclado Inalámbrico";
+            int quantity = 3;
+            decimal unitPrice = 899.00m;
+
+            string resumen = venta.CrearResumenVenta(productCode, productDescription, quantity, unitPrice);
+
+            Console.WriteLine("\nEjercicio 2: Resumen");
+            Console.WriteLine(resumen);
+
+            //------------ Problema 3 ------------
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey();
         }
     }
 }
